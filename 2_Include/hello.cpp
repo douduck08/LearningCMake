a79134a557a976a6a2b7b684406dee33f9174a67
douduck08 <lu10082000@gmail.com>
2023-01-01 18:58:54 +0800

    add include sample

---
+++ 2_Include/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include <iostream>
+
+void Hello::print_hello()
+{
+    std::cout << "Hello, CMake! Hello, included files!" << std::endl;
+}
---
+++ 2_Include/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hello.h"
+
+int main()
+{
+    Hello::print_hello();
+    return 0;
+}
